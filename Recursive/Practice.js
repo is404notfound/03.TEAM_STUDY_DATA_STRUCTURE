0255bab4259d4612539ae88dcc816863e1131058
is404notfound <is404notfound@gmail.com>
2022-09-29 12:30:14 +0900

    [Practice] Recursive 2

--- Recursive/Practice.js
+++ Recursive/Practice.js
@@ -6,3 +6,14 @@ function printRecursive(index = 1, target) {
   console.log(index);
   printRecursive(index + 1, MAX);
 }
+
+// printRecursive(1, MAX);
+
+/* 1부터 n까지 역순으로 출력 */
+function printReversed(index = 1, target) {
+  if (target < index) return;
+  console.log(target);
+  printReversed(index, target - 1);
+}
+
+// printReversed(1, MAX);
